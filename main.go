aff8c4f17b53e11f2bd6c77e97e1a1144effd000
Jeremy Adams <jeremy@dagger.io>
2024-02-26 16:07:28 -0800

    diff
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 
 type Foo struct{}
 
-func (m *Foo) Desc(ctx context.Context) string {
+func (m *Foo) Desc(ctx context.Context) (string, error) {
 	descGo, _ := dag.Git("https://github.com/jpadams/testgo.git").Branch("main").Tree().AsModule().Initialize().Description(ctx) 
 	descTs, _ := dag.Git("https://github.com/jpadams/testts.git").Branch("main").Tree().AsModule().Initialize().Description(ctx) 
 	descPy, _ := dag.Git("https://github.com/jpadams/testpy.git").Branch("main").Tree().AsModule().Initialize().Description(ctx) 
-	return descGo+"\n============\n"+descTs+"\n============\n"+descPy
+	return dag.Container().
+		From("alpine:latest").
+		WithWorkdir("src").
+		WithNewFile("go", ContainerWithNewFileOpts{Contents: descGo}).
+		WithNewFile("ts", ContainerWithNewFileOpts{Contents: descTs}).
+		WithNewFile("py", ContainerWithNewFileOpts{Contents: descPy}).
+		WithNewFile("xx", ContainerWithNewFileOpts{Contents: " xx "}). // sub xx in to see fail
+		WithExec([]string{"sh", "-c", "diff go ts && diff ts py"}).
+		Stdout(ctx)		
 }
